11dd24522f97be7c0dbfc348a17fe114b8cce546
bryangoodwork <bryan@goodwork.sg>
2023-07-18 10:56:06 +0800

    feat: transitions

--- apps/web/components/SessionCard.tsx
+++ apps/web/components/SessionCard.tsx
@@ -1,6 +1,13 @@
+import { Transition } from "@headlessui/react";
 import { SessionType } from "../app/session/page";
+import { useEffect, useState } from "react";
 
 export function SessionCard({ session }: { session: SessionType }) {
+  const [isShowing, setIsShowing] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsShowing(true);
+  }, []);
   //   try {
   //     if (session.error) {
   //       throw Error("Failed to load");
@@ -13,5 +20,17 @@ export function SessionCard({ session }: { session: SessionType }) {
   if (session.error) {
     throw Error("Failed to load");
   }
-  return <div className="bg-white w-full p-4 rounded-lg">Card</div>;
+  return (
+    <Transition
+      show={isShowing}
+      enter="transition-opacity duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-300"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="bg-white w-full p-4 rounded-lg">Card</div>
+    </Transition>
+  );
 }
